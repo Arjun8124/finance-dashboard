945e2b23d368303ad7ccb8eb845a4d8ae063b117
Arjun8124 <gupta.arjun.8124@gmail.com>
2026-06-16 22:31:04 +0530

    refactor: optimize MobileNavProvider and ThemeProvider with useCallback and useMemo

--- src/context/MobileNavProvider.tsx
+++ src/context/MobileNavProvider.tsx
@@ -1,16 +1,23 @@
-import { useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { MobileNavContext } from "./MobileNavContext";
 
 export function MobileNavProvider({ children }: { children: React.ReactNode }) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  
+
+  const openSidebar = useCallback(() => setIsSidebarOpen(true),[])
+  const closeSidebar = useCallback(() => setIsSidebarOpen(false), [])
+
+  const values = useMemo(() => {
+    return {
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar
+      }
+  },[isSidebarOpen,openSidebar,closeSidebar])
+
   return (
     <MobileNavContext.Provider
-      value={{
-        isSidebarOpen,
-        openSidebar: () => setIsSidebarOpen(true),
-        closeSidebar: () => setIsSidebarOpen(false),
-      }}
+      value={values}
     >
       {children}
     </MobileNavContext.Provider>
--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useCallback, useMemo } from "react";
 import useLocalStorage from "../hooks/useLocalStorage";
 import { ThemeContext } from "./ThemeContext";
 import { darkColors, lightColors } from "../constants/theme";
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const [isDark, setIsDark] = useLocalStorage<boolean>("theme-dark", true);
-
-  const toggleTheme = () => setIsDark((prev) => !prev);
   const colors = isDark ? darkColors : lightColors;
 
+  const toggleTheme = useCallback(() => setIsDark(prev => !prev),[setIsDark])
+
+  const values = useMemo(() => {
+    return { isDark, colors, toggleTheme}
+  },[isDark,colors,toggleTheme])
+
   return (
-    <ThemeContext.Provider value={{ isDark, colors, toggleTheme }}>
+    <ThemeContext.Provider value={values}>
       {children}
     </ThemeContext.Provider>
   );
